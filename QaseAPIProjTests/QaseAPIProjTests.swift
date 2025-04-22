c74fa8483066ff948267ef473f9d2441c8177d72
Sergey Ivanov <126597189+ivn-srg@users.noreply.github.com>
2025-04-22 23:48:20 +0600

    feature: Adding new unit test for API Manager

--- QaseAPIProjTests/QaseAPIProjTests.swift
+++ QaseAPIProjTests/QaseAPIProjTests.swift
@@ -10,6 +10,8 @@ import UIKit
 @testable import QaseAPIProj
 
 struct QaseAPIProjTests {
+    
+    let apiManager = APIMockManager.shared
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
@@ -22,12 +24,18 @@ struct QaseAPIProjTests {
         #expect(UIColor(hex: "#0000FF") == .blue)
         #expect(UIColor(hex: "#996633") == .brown)
     }
+    
+    @Test("API compose method checking")
+    func checkValidURL() async throws {
+        let projectUrl = apiManager.composeURL(for: .project, urlComponents: nil, queryItems: [.limit: 10])!
+        
+        #expect(projectUrl.absoluteString == "https://api.qase.io/v1/project?limit=10")
+    }
 
     @Test func checkFirstProjectTitle() async throws {
-        let jsonFileUrl = Bundle.main.url(forResource: "project_data", withExtension: "json")
-        let jsonData = try Data(contentsOf: jsonFileUrl!)
-        let projectsModel = try! JSONDecoder().decode(ProjectDataModel.self, from: jsonData)
-        let projects = projectsModel.result.entities
+        let dataURL = apiManager.composeURL(for: .project, urlComponents: nil)!
+        let projectsData = try await apiManager.performRequest(from: dataURL, method: .get, modelType: ProjectDataModel.self)
+        let projects = projectsData.result.entities
         
         #expect(projects.count == 2)
         #expect(projects[0].title == "Demo Project")
